62342b4dfbb7565a4ef93366a416b9beea9365ca
Vishen1104 <hannes.parkbrandberg@skola.taby.se>
2025-05-28 00:29:21 +0200

    Meny och email

--- projekt.js
+++ projekt.js
@@ -1,1 +1,34 @@
-// 
+document.addEventListener('DOMContentLoaded', function() {
+      console.log('Menu page loaded successfully');
+    });
+  
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".kontakt-form form");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); 
+    alert("Ditt mail har skickats");
+    
+  });
+});
+document.addEventListener('DOMContentLoaded'), () => {
+  const cartCount = document.getElementById('cart-count');
+  const priceTags = document.querySelectorAll('.price-tag');
+}
+
+const cartCount = document.getElementById('cart-count');
+const burgerImages = document.querySelectorAll('burger.png');
+
+let cartItems = 0;
+
+burgerImages.forEach(img => {
+  img.addEventListener('click', () => {
+    
+    cartItems++;
+    cartCount.textContent = cartItems;
+
+    
+    img.style.transform = 'scale(1.2)';
+    setTimeout(() => img.style.transform = 'scale(1)', 200);
+  });
+});
